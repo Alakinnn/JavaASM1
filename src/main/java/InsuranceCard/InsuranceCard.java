81a03d32dc91af44b37ccfcd0efde3fea287dd8b
Alakinnn <s3978452@rmit.edu.vn>
2024-03-20 18:20:38 +0700

    update InsuranceCard attribtues

--- src/main/java/InsuranceCard/InsuranceCard.java
+++ src/main/java/InsuranceCard/InsuranceCard.java
@@ -1,11 +1,12 @@
 package InsuranceCard;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 public class InsuranceCard {
     private String cardNumber;
     private String cardHolder;
     private String policyOwner;
-    private Date expirationDate;
+    private LocalDate expirationDate;
 
 }
