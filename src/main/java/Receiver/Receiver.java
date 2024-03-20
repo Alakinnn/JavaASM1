00afde3d266c9c23fcbbbb54c54b795ad06e9c59
Alakinnn <s3978452@rmit.edu.vn>
2024-03-20 18:10:51 +0700

    change class name

--- src/main/java/Receiver/Receiver.java
+++ src/main/java/Receiver/Receiver.java
@@ -1,11 +1,11 @@
 package Receiver;
 
-public class ReceiverInfo {
+public class Receiver {
     private String bankName;
     private String accountName;
     private String accountNumber;
 
-    public ReceiverInfo(String bankName, String accountName, String accountNumber) {
+    public Receiver(String bankName, String accountName, String accountNumber) {
         this.bankName = bankName;
         this.accountName = accountName;
         this.accountNumber = accountNumber;
