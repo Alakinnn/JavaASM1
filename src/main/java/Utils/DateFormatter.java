c0bc2ba3d121ea1570cb208cc22f65cb7c03654e
Alakinnn <s3978452@rmit.edu.vn>
2024-03-20 17:22:18 +0700

    add date formatter util class

---
+++ src/main/java/Utils/DateFormatter.java
@@ -0,0 +1,23 @@
+package Utils;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class DateFormatter {
+    public static LocalDate parseDate(String dateString) {
+        String[] patterns = {
+                "dd-MM-yyyy", "MM-dd-yyyy", "yyyy-MM-dd",  // With hyphens
+                "ddMMyyyy", "MMddyyyy", "yyyyMMdd"         // Without hyphens
+        };
+
+
+        for (String pattern : patterns) {
+            try {
+                return LocalDate.parse(dateString, DateTimeFormatter.ofPattern(pattern));
+            } catch (DateTimeParseException ignored) {
+            }
+        }
+        throw new DateTimeParseException("Unable to parse date", dateString, 0);
+    }
+}
