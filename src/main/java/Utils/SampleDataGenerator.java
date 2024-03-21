7984d36398402de346fe70e1b4d5887ed5fb06fe
Alakinnn <s3978452@rmit.edu.vn>
2024-03-22 00:52:24 +0700

    change sample data from automatic to manual

---
+++ src/main/java/Utils/SampleDataGenerator.java
@@ -0,0 +1,18 @@
+package Utils;
+
+import Claim.Claim;
+import Customer.*;
+import InsuranceCard.InsuranceCard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SampleDataGenerator {
+    private final static List<Customer> sampleCustomers = new ArrayList<>();
+    private final static List<InsuranceCard> sampleCards = new ArrayList<>();
+    private final static List<Claim> sampleClaims = new ArrayList<>();
+
+    public SampleDataGenerator() {
+
+    }
+}
