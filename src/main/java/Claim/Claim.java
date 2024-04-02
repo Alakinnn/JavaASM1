f820836a0cf9ce047bea80ca1cc9dce878e0e015
Alakinnn <117524098+Alakinnn@users.noreply.github.com>
2024-04-02 19:59:07 +0700

    add javadoc for claim

--- src/main/java/Claim/Claim.java
+++ src/main/java/Claim/Claim.java
@@ -1,5 +1,9 @@
 package Claim;
 
+/**
+ * @author <Duong Tran Minh Hoang - S3978452>
+ */
+
 import Database.Recordable;
 import Receiver.Receiver;
 import Utils.DateFormatter;
@@ -9,9 +13,15 @@ import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.*;
 
+/**
+ * Represents a claim made by an insured person.
+ * Implements Recordable and Serializable interfaces.
+ */
 public class Claim implements Recordable, Serializable {
+
     @Serial
     private static final long serialVersionUID = 987654321L;
+
     private static final Set<String> existingIds = new HashSet<>();
     private String id = "f";
     private final LocalDate claimDate;
@@ -22,6 +32,19 @@ public class Claim implements Recordable, Serializable {
     private final double claimAmount;
     private Status status;
     private final Receiver receiver;
+
+    /**
+     * Constructor for creating a Claim object.
+     *
+     * @param insuredPersonId   The ID of the insured person.
+     * @param cardNumber        The card number associated with the claim.
+     * @param examDate          The date of the examination related to the claim.
+     * @param claimAmount       The amount of the claim.
+     * @param statusOrdinal     The ordinal value of the claim status.
+     * @param receiverBank      The bank associated with the claim receiver.
+     * @param receiverName      The name of the claim receiver.
+     * @param receiverBankNumber The bank account number of the claim receiver.
+     */
 
     public Claim(String insuredPersonId, String cardNumber, String examDate, double claimAmount, int statusOrdinal, String receiverBank, String receiverName, String receiverBankNumber) {
         this.id = setID();
@@ -34,29 +57,59 @@ public class Claim implements Recordable, Serializable {
         this.receiver = new Receiver(receiverBank, receiverName, receiverBankNumber);
     }
 
+    /**
+     * Gets the ID of the insured person associated with the claim.
+     *
+     * @return The ID of the insured person.
+     */
     public String getInsuredPersonId() {
         return insuredPersonId;
     }
 
+    /**
+     * Gets the list of documents related to the claim.
+     *
+     * @return The list of documents.
+     */
     public List<String> getDocuments() {
         return documents;
     }
 
+    /**
+     * Adds a document to the list of documents related to the claim.
+     *
+     * @param document The document to be added.
+     */
     public void addDocument(String document) {
         documents.add(document);
     }
 
+    /**
+     * Sets the status of the claim.
+     *
+     * @param statusOrdinal The ordinal value of the claim status.
+     */
     public void setStatus(int statusOrdinal) {
         if (statusOrdinal != status.ordinal()) {
             this.status = Status.values()[statusOrdinal];
         }
     }
 
+    /**
+     * Retrieves the ID of the claim.
+     *
+     * @return The ID of the claim.
+     */
     @Override
     public String getID() {
         return this.id;
     }
 
+    /**
+     * Generates and sets a unique ID for the claim.
+     *
+     * @return The generated ID.
+     */
     @Override
     public String setID() {
         String ranId;
@@ -68,6 +121,11 @@ public class Claim implements Recordable, Serializable {
         return ranId;
     }
 
+    /**
+     * Returns a string representation of the claim object.
+     *
+     * @return A string containing the claim details.
+     */
     @Override
     public String toString() {
         return "Claim ID: " + id + "\n" +
@@ -80,5 +138,4 @@ public class Claim implements Recordable, Serializable {
                 "Receiver: " + receiver + "\n" +
                 "Number of Documents: " + getDocuments().size() + "\n";
     }
-
 }
